5ad2faac8a14db91cbbdb4e02fd005685a118430
Fukusan64 <ikishikawa64@gmail.com>
2019-12-28 03:48:28 +0900

    v0.2.0かなと思った

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ window.onload = async () => {
     const terminal = new Terminal('terminal');
     const shell = new Shell(
         terminal,
-        'v0.1.1',
+        'v0.2.0',
         (out, isError, user) => {
             out(`${user}@pc_hoge: `, {color: 'lime'});
             out('[', {color: 'cyan'});
