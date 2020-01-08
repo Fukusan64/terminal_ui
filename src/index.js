02b1b1a628fb70f026e4c5625fe9ad88280ecb49
Fukusan64 <ikishikawa64@gmail.com>
2020-01-08 13:56:00 +0900

    v0.4.1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ window.onload = async () => {
     const terminal = new Terminal('terminal');
     const shell = new Shell(
         terminal,
-        'v0.4.0',
+        'v0.4.1',
         (out, isError, user) => {
             out(`${user}@pc_hoge: `, {color: 'lime'});
             out('[', {color: 'cyan'});
